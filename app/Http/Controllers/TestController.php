b79062aff1a313843ec78344dec6e3b919953a4a
x-treme <yurchenko-inform@yandex.ru>
2024-08-14 21:19:33 +0300

    add oparator if

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,30 +15,32 @@ class TestController extends Controller
             'language' => ['HTML', 'CSS', 'SCSS', 'Laravel', 'JavaScript', 'PHP', 'Ruby']
         ]);
     }
-/*
-    private function getAge($user){
-        $diff = date( 'Ymd' ) - date( 'Ymd', strtotime($user['birthDate']) );
-        return substr( $diff, 0, -4 );
+
+    private function getDateRegister($user){
+        // получить год регистрации пользователя, записать в массив $mathes
+        $regex = '/([0-9]{4})/';
+        preg_match($regex, $user, $mathes);
+        return $mathes[0];
     }
-*/
+
     public function page(string $id, Request $request){
         $url = 'https://jsonplaceholder.org/users';
         // http - фасад, для получения данных по сети
         // получение с задержкой 5 с.
         $data = Http::timeout(5) -> get($url)->json();
-        //echo '<pre>';
-        // print_r($data);
-        //echo '</pre>';
-       /* 
-        foreach($data as $nu => $user){
-            $data[$nu]['age'] = $this -> getAge($user);
-            
-        }*/
+          
+         foreach($data as $key => $user){
+            // найти в массиве данных дату регистрации каждого пользователя
+            // и записать в новое поле каждого элемента массива
+            foreach ($user as $userkey => $item){
+               if($userkey === 'login') $data[$key]['register'] =  $this-> getDateRegister($item['registered']);
+            }
+           }
+        
         return view('test.page', [
             'id' => $id,
             'data' => $data,
             'url' => $request->url(),
-            
         ]);
     }
 
--- resources/views/test/page.blade.php
+++ resources/views/test/page.blade.php
@@ -6,10 +6,10 @@
     <title>page</title>
 </head>
 <body>
-    <p>{{$id}}</p>
+      <!--<p>{{$id}}</p>-->
          <pre> 
-       <!--вывести через print_r данные из json {{print_r( $data ) }}
-        </pre> -->
+       <!-- {{print_r( $data ) }}-->
+        <!--</pre>--> 
         <table>
         <tr>
         <th>First Name</th>
@@ -26,8 +26,16 @@
         </tr>
        <tbody>
        @foreach( $data as $item)
-           <tr>
-            <td>{{$item['firstname']}}</td>
+       <!--Если дата более 4-х лет давности, то не выводим в таблицу-->
+       @if((int)$item['register'] < ((int)date("Y"))-4 )
+        @continue
+        @endif
+         <!--Если строка таблицы четная, то красить в серый цвет-->
+         @if (!$loop->even && !((int)$item['register'] < ((int)date("Y"))-2))
+            <tr @style([
+            'background-color:#cacfd2',
+            ])>
+            <td >{{$item['firstname']}}</td>
             <td>{{$item['lastname']}}</td>
             <td>{{$item['email']}}</td>
             <td>{{$item['birthDate']}}</td>
@@ -39,8 +47,42 @@
             <td>{{$item['website']}}</td>
             <td>{{$item['company']['name']}}</td>
             </tr>
+           <!--Если дата регистраци 2-х летней давности, то красить в красный цвет-->  
+          @elseif ((int)$item['register'] < ((int)date("Y"))-2)
+          <tr @style([
+          'background-color:red',
+           ])>
+            <td >{{$item['firstname']}}</td>
+            <td>{{$item['lastname']}}</td>
+            <td>{{$item['email']}}</td>
+            <td>{{$item['birthDate']}}</td>
+            <td>{{$item['login']['registered']}}</td>
+            <td>{{$item['address']['city']}}</td>
+            <td>{{$item['address']['street']}}</td>
+            <td>{{$item['address']['suite']}}</td>
+            <td>{{$item['phone']}}</td>
+            <td>{{$item['website']}}</td>
+            <td>{{$item['company']['name']}}</td>
+            </tr>
+             <!--Иначе выводим как есть-->  
+           @else
+           <tr>
+            <td >{{$item['firstname']}}</td>
+            <td>{{$item['lastname']}}</td>
+            <td>{{$item['email']}}</td>
+            <td>{{$item['birthDate']}}</td>
+            <td>{{$item['login']['registered']}}</td>
+            <td>{{$item['address']['city']}}</td>
+            <td>{{$item['address']['street']}}</td>
+            <td>{{$item['address']['suite']}}</td>
+            <td>{{$item['phone']}}</td>
+            <td>{{$item['website']}}</td>
+            <td>{{$item['company']['name']}}</td>
+            </tr>
+            @endif
         @endforeach
        </tbody>
     </table>
+          </pre>
 </body>
 </html>
